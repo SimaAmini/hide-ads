98fc5cb31dee518102896ca17b657375bed23ce1
Sima Amini <sima.amini@connect2wow.com>
2024-01-06 14:13:08 +0330

    Update class name based on new changes

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const current = location;
 const hideElements = (elms) => elms.forEach(el => el.style.display = 'none');
 
 if(current.host === 'www.google.com'){
-    hideElements( document.querySelectorAll(".ads-ad") );
+    hideElements( document.querySelectorAll(".uEierd") );
 }
